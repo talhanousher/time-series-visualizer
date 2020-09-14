1c2b63164182f7cb9b6ceb6492a3d157458204e2
Talha Nousher Ali <talha@sudofy.com>
2020-09-14 20:54:36 +0500

    visualization using line graph

---
+++ time_series_visualizer.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
+
+# Import data (Make sure to parse dates. Consider setting index column to 'date'.)
+df = pd.read_csv('data.csv')
+df['date'] = pd.to_datetime(df['date'])
+# Clean data
+df = df[(df.value >= df.value.quantile(.025)) &
+        (df.value <= df.value.quantile(.975))]
+# print(df)
+
+
+def draw_line_plot():
+    # Draw line plot
+    fig, _ = plt.subplots()
+    plt.plot(df['date'], df['value'], color="r")
+    # Save image and return fig (don't change this part)
+    fig.savefig('line_plot.png')
+    return fig
+
+
+def draw_bar_plot():
+    # Copy and modify data for monthly bar plot
+    df_bar = None
+
+    # Draw bar plot
+
+    # Save image and return fig (don't change this part)
+    fig.savefig('bar_plot.png')
+    return fig
+
+
+def draw_box_plot():
+    # Prepare data for box plots (this part is done!)
+    df_box = df.copy()
+    df_box.reset_index(inplace=True)
+    df_box['year'] = [d.year for d in df_box.date]
+    df_box['month'] = [d.strftime('%b') for d in df_box.date]
+
+    # Draw box plots (using Seaborn)
+
+    # Save image and return fig (don't change this part)
+    fig.savefig('box_plot.png')
+    return fig
